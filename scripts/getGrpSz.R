b4b69317cf015ebd46c846444894606e418500f0
Eiren Jacobson <eiren.jacobson@gmail.com>
2026-04-28 11:59:14 -0700

    Script to get observed group sizes from data

---
+++ scripts/getGrpSz.R
@@ -0,0 +1,17 @@
+library(dplyr)
+
+si.humpback <- read.csv("./Data/sightings.csv") %>%
+  filter(spcode == 22) %>%
+  select(group_size)
+
+si.humpback <- na.omit(si.humpback$group_size)
+
+save(si.humpback, file = "./distance/humpback_grpsz.RData")
+
+si.pwsd <- read.csv("./Data/sightings.csv") %>%
+  filter(spcode == 76) %>%
+  select(group_size) 
+
+si.pwsd <- na.omit(si.pwsd$group_size)
+
+save(si.pwsd, file = "./distance/pwsd_grpsz.RData")
